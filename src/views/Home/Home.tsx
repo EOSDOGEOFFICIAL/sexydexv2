1dbcdd2929c5c3d42b1377f4cc7b161ee0d09a11
DEV_CAKE <88570979+EOSDOGEOFFICIAL@users.noreply.github.com>
2021-08-31 17:46:09 -0600

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -90,9 +90,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Ape Finance')}
+          {TranslateString(576, 'WifeSwap')}
         </Heading>
-        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'Soon the #1 AMM and yield farm on Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
