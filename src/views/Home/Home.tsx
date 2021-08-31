7a44031cb4e5e03e5f04c9f27fe2e87c953f1262
DEV_CAKE <88570979+EOSDOGEOFFICIAL@users.noreply.github.com>
2021-08-31 14:55:11 -0600

    Update Home.tsx

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -13,7 +13,7 @@ import { Heading, Text, BaseLayout } from '../../components/Pancakeswap/uikit/sr
 
 const Hero = styled.div`
   align-items: center;
-  background-image: url('/images/home/donel.png'), url('/images/home/donel.png');
+  background-image: url('/images/home/left.png'), url('/images/home/right.png');
   background-position: left top, right top;
   background-size: 80px 100px;
   background-repeat: no-repeat;
