8968791bd116b00aae11b8932eb21e7bef02c9bb
DEV_CAKE <88570979+EOSDOGEOFFICIAL@users.noreply.github.com>
2021-09-01 15:42:23 -0600

    Update PrizesWonContent.tsx

--- src/views/Lottery/components/YourPrizesCard/PrizesWonContent.tsx
+++ src/views/Lottery/components/YourPrizesCard/PrizesWonContent.tsx
@@ -82,7 +82,7 @@ const PrizesWonContent: React.FC<PrizesWonContentProps> = ({ onSuccess }) => {
               {winnings}
             </Heading>
             <Heading as="h4" size="lg">
-              APES
+              Waifu
             </Heading>
           </WinningsWrapper>
         </>
